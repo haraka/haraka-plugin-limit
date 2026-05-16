f0a0781fe6846d64610e62b5464dc4a28fbff343
Matt Simerson <matt@tnpi.net>
2026-05-15 22:09:57 -0700

    change: treat Address.address as property
    - pairs with https://github.com/haraka/Haraka/issues/3564

--- test/helpers.js
+++ test/helpers.js
@@ -3,7 +3,7 @@
 
 const path = require('node:path')
 
-const { Address } = require('address-rfc2821')
+const { Address } = require('@haraka/email-address')
 const fixtures = require('haraka-test-fixtures')
 
 // plugin with limit.ini loaded from test/config/, registered, no redis
